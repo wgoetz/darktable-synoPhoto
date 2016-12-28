13f4eae5bb215b1e7341c43402ccf3abd399990f
Wolfgang Goetz <wolfgang.ztoeg@web.de>
2016-12-28 10:03:13 +0100

    check for quirks

--- do-cache2album.sh
+++ do-cache2album.sh
@@ -5,13 +5,15 @@ dt_cache=$(ls -d ~/.cache/darktable/mipmaps-*.d)
 mip_stage=4
 album="$HOME/Pictures/dt-mip/$mip_stage"
 
-darktable-generate-cache -m $mip_stage 
+#darktable-generate-cache -m $mip_stage 
 
 while IFS="|" read n i d f;do
 	[[ "$d" =~ [[:digit:]] ]] || continue
 
 	nb=${n%%.*}
 	if="$dt_cache/$mip_stage/$i.jpg"
+	[ -f $if ] || continue
+
 	IFS=":" read Y M r <<< "$d"
 	fb=${f##*/}
 
@@ -19,6 +21,9 @@ while IFS="|" read n i d f;do
 	of="$od/$nb.jpg"
 	
 	[ -d "$od" ] || mkdir -pv "$od" 
-	[ -f "$of" ] || { cp -p $if "$of"; echo -n .; } 
+	[ -f "$of" ] || cp -pv $if "$of"
+	[ "$if" -nt "$of" ] &&  { echo TIMESTAMP NT $i $if $of; stat "$if" "$of"; diff  "$if" "$of"; echo; }
+	[ "$if" -ot "$of" ] && echo TIMESTAMP OT $i $if $of
+	#diff "$if" "$of"
 done < <(sqlite3 ~/.config/darktable/library.db "select filename,images.id,datetime_taken,film_rolls.folder from images inner join film_rolls on images.film_id=film_rolls.id;")
 
